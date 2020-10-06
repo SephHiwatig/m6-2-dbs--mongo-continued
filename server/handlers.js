2a570f0aba11aceb5bb2decf0983034bffb269b9
WilfredoJoseph Hiwatig <jhiwatig.dev@gmail.com>
2020-10-05 20:57:19 -0400

    Finished excercise 2

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,70 @@
-'use strict';
+"use strict";
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const { MongoClient } = require("mongodb");
 
-const getSeats = async (req, res) => {};
+let state;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const getSeats = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+
+  await client.connect();
+
+  const db = client.db("ticket");
+
+  const seats = await db.collection("seats").find().toArray();
+
+  client.close();
+
+  if (!state) {
+    state = {
+      bookedSeats: randomlyBookSeats(30),
+    };
+  }
+  const seatObj = {};
+  seats.forEach((seat) => {
+    seatObj[seat._id] = {
+      price: seat.price,
+      isBooked: seat.isBooked,
+    };
+  });
+
+  return res.json({
+    seats: seatObj,
+    bookedSeats: state.bookedSeats,
+    numOfRows: 8,
+    seatsPerRow: 12,
+  });
+};
+
+//////// HELPERS
+const NUM_OF_ROWS = 8;
+const SEATS_PER_ROW = 12;
+
+const getRowName = (rowIndex) => {
+  return String.fromCharCode(65 + rowIndex);
+};
+
+const randomlyBookSeats = (num) => {
+  const bookedSeats = {};
+
+  while (num > 0) {
+    const row = Math.floor(Math.random() * NUM_OF_ROWS);
+    const seat = Math.floor(Math.random() * SEATS_PER_ROW);
+
+    const seatId = `${getRowName(row)}-${seat + 1}`;
+
+    bookedSeats[seatId] = true;
+
+    num--;
+  }
+
+  return bookedSeats;
+};
 
 module.exports = { getSeats };
